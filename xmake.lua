2f2eabf88204a863a20653c2a3897f3504802518
Zijian Zhang <futrime@outlook.com>
2023-10-29 16:55:03 +0800

    build: add ScriptX backend

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,14 @@
 add_rules("mode.debug", "mode.release")
 
-target("LeviScript")
+add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_requires("scriptx", {configs = {backend = "lua"}})
+
+target("leviscript")
     set_kind("shared")
+    set_languages("cxx20")
+
     add_files("src/**.cc")
+    add_includedirs("include")
+    add_packages(
+        "scriptx"
+    )
