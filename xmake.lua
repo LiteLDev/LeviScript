946d858315b986904158e2fd6e8d302ccb78ece9
Futrime <futrime@outlook.com>
2023-10-30 19:57:05 +0800

    ci: allow multi-platform build

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,18 @@
-add_rules("mode.debug", "mode.release")
-
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
-add_requires("scriptx", {configs = {backend = "lua"}})
+add_requires("scriptx",
+    {configs = {backend = get_config("backend")}})
+
+option("backend")
+    set_values("lua", "nodejs", "python310", "quickjs")
+    set_default("lua")
 
 target("leviscript")
-    set_kind("shared")
-    set_languages("cxx20")
-
     add_files("src/**.cc")
     add_includedirs("include")
     add_packages(
         "scriptx"
     )
+    add_rules("mode.debug", "mode.release")
+    set_basename("leviscript_$(backend)")
+    set_kind("shared")
+    set_languages("cxx20")
