7b5e6bf513d02ab1e56cf4bde83f75ed0530716b
Futrime <futrime@outlook.com>
2023-11-24 08:52:48 +0800

    feat: use #pragma once

--- include/Plugin.h
+++ include/Plugin.h
@@ -1,5 +1,4 @@
-#ifndef LEVILAMINA_EXAMPLE_PLUGIN_PLUGIN_H_
-#define LEVILAMINA_EXAMPLE_PLUGIN_PLUGIN_H_
+#pragma once
 
 #include <string_view>
 
@@ -42,5 +41,3 @@ private:
 
     ll::plugin::Plugin& getSelf() const;
 };
-
-#endif // LEVILAMINA_EXAMPLE_PLUGIN_PLUGIN_H_
