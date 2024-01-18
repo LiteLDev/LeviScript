dc7aca50368b4d2cc8a2e0e40c4e609450a7d28b
Zijian Zhang <futrime@outlook.com>
2024-01-18 20:34:15 +0800

    build: upgrade required LeviLamina version to 0.5.0

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,7 @@
+add_rules("mode.debug", "mode.release", "mode.releasedbg")
+
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
-add_requires("levilamina 0.2.1")
+add_requires("levilamina 0.5.0")
 add_requires("scriptx", {configs = {backend = get_config("backend")}})
 
 if not has_config("vs_runtime") then
@@ -11,6 +13,13 @@ option("backend")
     set_values("lua", "nodejs", "python310", "quickjs")
 
 target("plugin")
+    add_cxflags(
+        "/EHa",
+        "/utf-8"
+    )
+    add_defines(
+        "_HAS_CXX23=1" -- To enable C++23 features
+    )
     add_files(
         "src/**.cpp"
     )
@@ -21,15 +30,10 @@ target("plugin")
         "levilamina",
         "scriptx"
     )
-    add_rules(
-        "mode.debug",
-        "mode.release",
-        "mode.releasedbg"
-    )
     add_shflags(
         "/DELAYLOAD:bedrock_server.dll"
     )
     set_basename("leviscript-$(backend)")
+    set_exceptions("none")
     set_kind("shared")
     set_languages("cxx20")
-    set_strip("all")
